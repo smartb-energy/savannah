47d44451fcb01a9765813dda51c1683c9461713c
Blake Irvin <dev+blake.irvin@smartb.eu>
2017-08-21 17:56:48 +0200

    move triton configs to separate file

--- triton.tf
+++ triton.tf
@@ -1,1 +1,6 @@
 provider "triton" {
+    account      = "smartb"
+    key_material = "${file("~/.ssh/id_rsa")}"
+    key_id       = "93:50:bb:2b:dd:bf:68:6a:0f:b7:cf:f8:15:1d:e9:b9"
+    url = "https://eu-ams-1.api.joyentcloud.com"
+}
