71e5f736f354b749dba72bf92770eda618413514
Blake Irvin <dev+blake.irvin@smartb.eu>
2017-08-21 17:54:16 +0200

    move Triton configs into separate file

--- hosts.tf
+++ hosts.tf
@@ -1,9 +1,3 @@
-provider "triton" {
-    account      = "smartb"
-    key_material = "${file("~/.ssh/id_rsa")}"
-    key_id       = "93:50:bb:2b:dd:bf:68:6a:0f:b7:cf:f8:15:1d:e9:b9"
-    url = "https://eu-ams-1.api.joyentcloud.com"
-}
 
 resource "triton_machine" "dev-postgres-permanent-peer" {
     image   = "7b5981c4-1889-11e7-b4c5-3f3bdfc9b88b"
