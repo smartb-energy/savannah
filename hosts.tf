f23ee10312bae25c08a75e8b7a242ffb4568b250
Blake Irvin <dev+blake.irvin@smartb.eu>
2017-08-22 18:38:48 +0200

    WIP: refactor to use `core/postgresql` and simpler configs in general
    there is still a bug regarding how `hab svc status` reports state of
    postgres

--- hosts.tf
+++ hosts.tf
@@ -1,71 +1,38 @@
-resource "triton_machine" "dev-permanent-peer" {
-    image   = "7b5981c4-1889-11e7-b4c5-3f3bdfc9b88b"
-    name    = "dev-permanent-peer"
-    package = "g4-highcpu-256M"
-    connection {
-        user = "root"
-        host = "${self.primaryip}"
-        private_key = "${file("~/.ssh/id_rsa")}"
-    }
-    nic = {
-      network = "ccccb251-1b24-457a-8b46-459e2199882e"
-    }
-
-    provisioner "file" {
-      source      = "supervisor.service"
-      destination = "/etc/systemd/system/supervisor.service"
-    }
-
-    provisioner "remote-exec" {
-        inline = [
-            "hostname ${self.name}",
-            "useradd --system hab",
-            "curl -sL https://raw.githubusercontent.com/habitat-sh/habitat/master/components/hab/install.sh | sudo bash",
-            "systemctl daemon-reload && systemctl start supervisor"
-        ]
-    }
-}
-
 resource "triton_machine" "dev-postgres" {
-    count = 3
-    depends_on = ["triton_machine.dev-permanent-peer"]
-    image   = "7b5981c4-1889-11e7-b4c5-3f3bdfc9b88b"
-    name    = "dev-postgres-${count.index}"
-    package = "g4-highcpu-512M"
-    connection {
-        user = "root"
-        host = "${self.primaryip}"
-        private_key = "${file("~/.ssh/id_rsa")}"
-    }
-    nic = {
-      network = "ccccb251-1b24-457a-8b46-459e2199882e"
-    }
+  count = 3
+  image   = "7b5981c4-1889-11e7-b4c5-3f3bdfc9b88b"
+  name    = "dev-postgres-${count.index}"
+  package = "g4-highcpu-2G"
+  connection {
+    user = "root"
+    host = "${self.primaryip}"
+    private_key = "${file("~/.ssh/id_rsa")}"
+  }
 
-    provisioner "file" {
-      source      = "supervisor.service"
-      destination = "/etc/systemd/system/supervisor.service"
-    }
+  provisioner "file" {
+    destination = "$HOME/supervisor.service"
+    content = <<EOF
+[Unit]
+Description=The Habitat Supervisor
 
-    provisioner "remote-exec" {
-        inline = [
-            "hostname ${self.name}",
-            "useradd --system hab",
-            "curl -sL https://raw.githubusercontent.com/habitat-sh/habitat/master/components/hab/install.sh | sudo bash",
-            "systemctl daemon-reload && systemctl start supervisor",
-            "HAB_NONINTERACTIVE=true hab pkg install core/postgresql",
-            "mkdir --parents /hab/svc/postgresql94 && chown --recursive hab /hab/svc/postgresql94"
-        ]
-    }
+[Service]
+ExecStart=/bin/hab sup run --peer ${triton_machine.dev-postgres.0.primaryip}
 
-    provisioner "file" {
-      source      = "user.toml"
-      destination = "/hab/svc/postgresql94/user.toml"
-    }
+[Install]
+WantedBy=default.target
+EOF
+  }
 
-    provisioner "remote-exec" {
-        inline = [
-            "hab start core/postgresql --group dev --topology leader --peer ${triton_machine.dev-permanent-peer.primaryip}",
-            "timeout 60 bash -c 'until hab svc status core/postgresql | grep state:up; do echo waiting for core/postgresql to come up...; sleep 1; done'"
-        ]
-    }
+  provisioner "remote-exec" {
+    inline = [
+      "useradd --system hab",
+      "curl -sL https://raw.githubusercontent.com/habitat-sh/habitat/master/components/hab/install.sh | bash",
+      "hab pkg install core/hab-sup",
+      "mv $HOME/supervisor.service /etc/systemd/system/supervisor.service",
+      "systemctl daemon-reload",
+      "systemctl start supervisor",
+      "systemctl enable supervisor",
+      "hab svc load core/postgresql --group dev --topology leader"
+    ]
+  }
 }
