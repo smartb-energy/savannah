eb82f50518a2d220f4793278137222a7331824ed
Blake Irvin <dev+blake.irvin@smartb.eu>
2017-08-22 19:28:59 +0200

    document and populate Triton `TF_VAR_`s

--- triton.tf
+++ triton.tf
@@ -1,6 +1,18 @@
+# We use Joyent's Triton cloud for internal testing. Community collaborators
+# can set up a free account via http://lpage.joyent.com/Triton-Free-Trial.html
+# To get the correct TF_VAR_ environment variables for these Terraform configs,
+# first install the Triton cli tools for your account like `npm install -g triton`
+#
+# Your `.bash_profile` entry might look like:
+#
+# eval "$(triton env)"
+# export TF_VAR_triton_key_id=$SDC_KEY_ID
+# export TF_VAR_triton_account=$SDC_ACCOUNT
+# export TF_VAR_triton_url=$SDC_URL
+
 provider "triton" {
-    account      = "smartb"
+    account      = "${var.triton_account}"
     key_material = "${file("~/.ssh/id_rsa")}"
-    key_id       = "93:50:bb:2b:dd:bf:68:6a:0f:b7:cf:f8:15:1d:e9:b9"
-    url = "https://eu-ams-1.api.joyentcloud.com"
+    key_id       = "${var.triton_key_id}"
+    url = "${var.triton_url}"
 }
---
+++ variables.tf
@@ -0,0 +1,7 @@
+# Declaring an empty variable will result in a prompt at the Terraform CLI.
+# Variables can also be imported from the *nix environment if they have the
+# `TF_VAR_` prefix. For example, `export TF_VAR_instance_key_name=$USER`
+
+variable "triton_key_id" {}
+variable "triton_account" {}
+variable "triton_url" {}
